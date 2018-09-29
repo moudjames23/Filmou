2ebf255c4ddf2bff3355f88f4d6664d828f22db5
Moudjames23 <moud23diallo@outlook.com>
2018-09-29 11:17:58 +0000

    Initial Cheking

---
+++ app/src/main/java/com/moudjames23/filmou/MainActivity.java
@@ -0,0 +1,35 @@
+package com.moudjames23.filmou;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.rv_film)
+    RecyclerView rvFilm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+
+    }
+
+
+}
